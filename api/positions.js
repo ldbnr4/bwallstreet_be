ba6edbe55a8abf3aa9c935b25a2aadf4e5d1bf85
Lorenzo Boyice <lboyice@google.com>
2020-10-06 06:31:09 -0600

    back to the basics

--- api/positions.js
+++ api/positions.js
@@ -21,67 +21,73 @@ function newFun(callback) {
         console.log(qty);
         console.log(amntPaid);
         console.log("Paid: " + amntPaid);
-        client.connect((err) => {
-          // var dbUsers = [];
-          console.log("Connected successfully to database");
+        // client.connect((err) => {
+        //   // var dbUsers = [];
+        //   console.log("Connected successfully to database");
 
-          db = client.db(process.env.db_name);
+        //   db = client.db(process.env.db_name);
 
-          db.collection("positions").insertOne({
-            _id: body.results[0].account_number,
-            positions: body.results
-          });
+        //   db.collection("positions").insertOne({
+        //     _id: body.results[0].account_number,
+        //     positions: body.results
+        //   });
 
-          // // Removes any existing entries in the users collection
-          // db.collection("positions").deleteMany(
-          //   { name: { $exists: true } },
-          //   function (err, r) {
-          //     for (var i = 0; i < users.length; i++) {
-          //       // loop through all default users
-          //       dbUsers.push({ name: users[i] });
-          //     }
-          //     // add them to users collection
-          //     db.collection("positions").insertMany(dbUsers, function (
-          //       err,
-          //       r
-          //     ) {
-          //       console.log("Inserted initial users");
-          //     });
-          //   }
-          // );
-        });
+        //   // // Removes any existing entries in the users collection
+        //   // db.collection("positions").deleteMany(
+        //   //   { name: { $exists: true } },
+        //   //   function (err, r) {
+        //   //     for (var i = 0; i < users.length; i++) {
+        //   //       // loop through all default users
+        //   //       dbUsers.push({ name: users[i] });
+        //   //     }
+        //   //     // add them to users collection
+        //   //     db.collection("positions").insertMany(dbUsers, function (
+        //   //       err,
+        //   //       r
+        //   //     ) {
+        //   //       console.log("Inserted initial users");
+        //   //     });
+        //   //   }
+        //   // );
+        // });
       }
     });
   });
 }
 
+// module.exports = (req, res) => {
+//   res.send("some");
+//   const MongoClient = require("mongodb").MongoClient;
+//   var db;
+
+//   const client = new MongoClient(process.env.mongodb_url, {
+//     useNewUrlParser: true
+//   });
+
+//   // res.send("surrender");
+//   // Connect to database and insert default users into users collection
+//   client.connect((err) => {
+//     // var dbUsers = [];
+//     console.log("Connected successfully to database");
+
+//     db = client.db(process.env.db_name);
+//     // if (req.method === "PUT") {
+//     //   newFun();
+//     // } else {
+//     db.collection("positions")
+//       .find({})
+//       .toArray(function (err, result) {
+//         if (err) throw err;
+//         // console.log(result);
+//         res.status(200);
+//         res.setHeader("Content-Type", "application/json");
+//         res.send(JSON.stringify(result));
+//       });
+//     // }
+//   });
+// };
+
 module.exports = (req, res) => {
-  const MongoClient = require("mongodb").MongoClient;
-  var db;
-
-  const client = new MongoClient(process.env.mongodb_url, {
-    useNewUrlParser: true
-  });
-
-  // res.send("surrender");
-  // Connect to database and insert default users into users collection
-  client.connect((err) => {
-    // var dbUsers = [];
-    console.log("Connected successfully to database");
-
-    db = client.db(process.env.db_name);
-    if (req.method === "PUT") {
-      newFun();
-    } else {
-      db.collection("positions")
-        .find({})
-        .toArray(function (err, result) {
-          if (err) throw err;
-          // console.log(result);
-          res.status(200);
-          res.setHeader("Content-Type", "application/json");
-          res.send(JSON.stringify(result));
-        });
-    }
-  });
+  // const { name = 'World' } = req.query
+  res.send(`Hello World!`);
 };
